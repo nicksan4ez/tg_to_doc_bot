171dae094bf8c78e5e8bac20cd520865fe1ff45f
nicksan4ez <san4ez.nick@gmail.com>
2026-02-16 00:14:54 +0300

    убрал обработку стикеров из фильтра неподдерживаемых медиа

--- bot.py
+++ bot.py
@@ -617,7 +617,7 @@ def main() -> None:
     application.add_handler(MessageHandler(filters.Document.ALL, handle_document))
     application.add_handler(
         MessageHandler(
-            (filters.PHOTO | filters.VIDEO | filters.AUDIO | filters.VOICE | filters.ANIMATION | filters.VIDEO_NOTE | filters.STICKER),
+            (filters.PHOTO | filters.VIDEO | filters.AUDIO | filters.VOICE | filters.ANIMATION | filters.VIDEO_NOTE),
             handle_unsupported_media,
         )
     )
